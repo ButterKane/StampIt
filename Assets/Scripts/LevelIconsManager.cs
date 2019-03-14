480d7a9fdd9a8acdd75eab761aab29bec7d5d54b
ETU\a.coteyangting <s-a-f-e-r@outlook.fr>
2019-03-14 18:19:30 +0100

    More UI

---
+++ Assets/Scripts/LevelIconsManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelIconsManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            gameObject.GetComponent<RectTransform>().localPosition += new Vector3(0, touch.deltaPosition.y, 0);
+        }
+    }
+}
